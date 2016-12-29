fd46bb811d53e008fddbbc0615d0f524ac4cb83d
franklinovitch <quentinbarb@gmail.com>
2016-12-29 16:38:29 +0100

    Adding vim config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bash_profile bash_aliases bashrc zshrc oh-my-zsh tmux.conf"    # list of files/folders to symlink in homedir
+files="bash_profile bash_aliases bashrc zshrc oh-my-zsh tmux.conf vimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
 
